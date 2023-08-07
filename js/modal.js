cc53b96948d579263046942df1a0e3a39255f1c9
JuanGuillermoPantoja <jgpantoja13@gmail.com>
2023-08-07 14:22:59 -0500

    update example trailer

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,17 @@
 const openModal = document.querySelector(".hero__cta");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".modal__close");
+const hideModal = document.querySelector(".movies");
 
 openModal.addEventListener("click", (e) => {
   e.preventDefault();
   modal.classList.add("modal--show");
+  hideModal.style.display = 'none';
 });
 
 closeModal.addEventListener("click", (e) => {
   e.preventDefault();
   modal.classList.remove("modal--show");
+  hideModal.style.display = 'block'
 });
+
